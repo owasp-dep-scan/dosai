93f509f151ac897490131223949010d0fd9a52ba
Tim Messing <141575989+timmyteo@users.noreply.github.com>
2023-12-11 16:23:22 -0700

    Fix Tests

---
+++ Dosai.TestData/FooBar.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+
+namespace FooBar
+{
+    public class Foo
+    {         
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Bar
+    {
+        public void bar()
+        {
+            
+        }
+
+        private void PrivateMethod()
+        {
+            
+        }
+    }
+}
---
+++ Dosai.TestData/HelloWorld.cs
@@ -0,0 +1,31 @@
+using System.Globalization;
+using System.Reflection;
+
+namespace HelloWorld
+{
+    public class Hello
+    {         
+        public static void elevate()
+        {
+            
+        }
+
+        public async Task Appreciate()
+        {
+            await Task.Delay(0);
+        }
+    }
+
+    public class World
+    {
+        public void shout()
+        {
+            
+        }
+
+        private void PrivateMethod()
+        {
+            
+        }
+    }
+}
